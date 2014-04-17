50820c121500b104f39ae479a4f336130f10b2b5
Lidia <lidiamcfreitas@gmail.com>
2014-04-17 11:08:19 +0100

    Added problem LeftAndRightHanded

---
+++ 250 LeftAndRightHandedDiv2.py
@@ -0,0 +1,13 @@
+
+import string
+
+class LeftAndRightHandedDiv2:
+    def count(self, S):
+        return S.count('RL')
+    
+#test = LeftAndRightHandedDiv2()
+#S = input("insert a row (x to close): ")
+#print(test.count(S))
+#while S!='x':
+    #S = input("insert a row (x to close): ")
+    #print(test.count(S))    
